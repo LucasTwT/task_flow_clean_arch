9c720ef0dc214a4b2ab3634f4587df5d0b8c984b
LucasTwT <lucastwt22@gmail.com>
2026-06-20 19:46:38 +0200

    SRP applied on the base code

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
-from user_service import UserService
+from user_service import *
+
 
 def main() -> None:
-    users: list[dict] = [{"id": "123", "name": "Lucas", "email": "lucas@gmail.com", "password": "1234"}]
-    service = UserService()
-    print("Get user: ", service.get_user("123", users))
-    print("Validate email: ",service.validate_email("adfadfadfa"))
-    print("Validate email:",service.validate_email("lucas@gmail.com"))
-    service.save_user("124", "Jose", "jose@gmail.com", "123123123",  users)
-    print("Validate password: ",service.validate_password("123"))
-    print("Validate password: ",service.validate_password("1231231"))
-    service.send_email("jose@gmail.com")
-    print("Get user: ",service.get_user("124", users))
+    validator = UserValidator()
+    repository = UserRepository()
+    notifier = UserNotifier()
+    repository.save_user(UserData(username="Lucas", email="lucas@gmail.com", password="123123123", id="123"))
+    print("Get user: ", repository.get_user("123"))
+    print("Validate email: ",validator.validate_email("adfadfadfa"))
+    print("Validate email:",validator.validate_email("lucas@gmail.com"))
+    repository.save_user(UserData(username="jose", email="jose@gmail.com", password="ndflkansdflakdnfald", id="124"))
+    print("Validate password: ",validator.validate_password("123"))
+    print("Validate password: ",validator.validate_password("1231231"))
+    notifier.send_email("jose@gmail.com")
+    print("Get user: ", repository.get_user("124"))
 
 if __name__ == "__main__":
     main()
--- src/user_service.py
+++ src/user_service.py
@@ -1,22 +1,42 @@
 import re
+from dataclasses import dataclass
+from typing import final
 
 
-class UserService:
-    def validate_email(self, email: str, pat: str = r".*@+.*") -> bool:
-        return True if re.fullmatch(pattern=pat, string=email) else False
-    
-    def validate_password(self, password: str, pat: str = r".*.") -> bool:
-        """Por si en el futuro se quiere añadir un patrón extra aparte de la longitud minima"""
-        return True if (re.fullmatch(pattern=pat, string=password) and len(password) > 6) else False
-    
-    def save_user(self, id: str, name: str, email: str, password: str, users: list[dict]) -> None:
-        users.append({"id": id, "name": name, "email": email, "password": password})
-    
-    def send_email(self, email) -> None:
+@dataclass
+class UserData:
+    id: str
+    username: str
+    email: str
+    password: str
+
+@final
+class UserValidator:
+    _PASSWORD_PATTERN = r".*."
+    _EMAIL_PATTERN = r".*@+.*"
+    _PASSWORD_MIN_LENGTH = 6
+
+    def validate_email(self, email: str) -> bool:
+        return True if re.fullmatch(pattern=self._EMAIL_PATTERN, string=email) else False
+
+    def validate_password(self, password: str) -> bool:
+        return True if (re.fullmatch(pattern=self._PASSWORD_PATTERN, string=password) and len(password) > self._PASSWORD_MIN_LENGTH) else False
+
+
+class UserRepository:
+    def __init__(self) -> None:
+        self.users: list[UserData] = []
+
+    def save_user(self, userdata: UserData) -> None:
+        self.users.append(userdata)
+
+    def get_user(self, id: str) -> UserData | None:
+        for user in self.users:
+            if user.id == id:
+                return user
+        return None
+
+
+class UserNotifier:
+    def send_email(self, email: str) -> None:
         print(f"Email enviado a {email}")
-
-    def get_user(self, id, users: list[dict]) -> dict:
-        for user in users:
-            if user["id"] == id:
-                return user            
-        return {}
